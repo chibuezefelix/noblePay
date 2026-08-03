c1ed4be89f4afd88b9d4a633f933e21312e5cca5
Chibueze  felix <33674336+leadpresence@users.noreply.github.com>
2026-08-03 02:30:40 +0100

    Conf(repo): Initial dependencies
    Application initial dependebcencies setup for the payment project

---
+++ src/main/java/com/opxl/noblepay/NoblepayApplication.java
@@ -0,0 +1,13 @@
+package com.opxl.noblepay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoblepayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NoblepayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/opxl/noblepay/NoblepayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.opxl.noblepay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoblepayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
