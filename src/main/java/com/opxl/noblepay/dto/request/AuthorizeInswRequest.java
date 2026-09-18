6baed23348d796cded75ef534657fa2306c96f8c
Chibueze  felix <33674336+leadpresence@users.noreply.github.com>
2026-09-18 15:40:51 +0100

    Feat(app): Added 3d secure  AuthorizeInswRequest
    Implemented model to handle  the authorizaion of request with3d secure eciflag

---
+++ src/main/java/com/opxl/noblepay/dto/request/AuthorizeInswRequest.java
@@ -0,0 +1,10 @@
+package com.opxl.noblepay.dto.request;
+
+public class AuthorizeInswRequest {
+    private String paymentId;
+    private String otp;
+    private String transactionId;
+    private  String eciFlag;
+    private  String authFlag;
+    private String md;
+}
