a91d9a6068fcf221cfdbc872896ee7a4f5a639cf
Chibueze  felix <33674336+leadpresence@users.noreply.github.com>
2026-09-23 16:47:15 +0100

    Chore(app):  Added Getter & Setters in the AuthorizeInswRequest model

--- src/main/java/com/opxl/noblepay/dto/request/AuthorizeInswRequest.java
+++ src/main/java/com/opxl/noblepay/dto/request/AuthorizeInswRequest.java
@@ -1,5 +1,10 @@
 package com.opxl.noblepay.dto.request;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
 public class AuthorizeInswRequest {
     private String paymentId;
     private String otp;
